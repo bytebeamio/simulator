c8c97845e9fc947069fb1f4d510d3e35d377730a
Devdutt Shenoi <devdutt@bytebeam.io>
2024-07-19 11:54:15 +0530

    log topic that couldn't be updated to network

--- src/serializer.rs
+++ src/serializer.rs
@@ -1,6 +1,6 @@
-use log::{debug, error};
+use log::error;
 use rumqttc::{AsyncClient, QoS};
-use tokio::{sync::mpsc::Receiver, time::Instant};
+use tokio::sync::mpsc::Receiver;
 
 use crate::data::PayloadArray;
 
@@ -14,17 +14,12 @@ pub struct Serializer {
 impl Serializer {
     pub async fn start(&mut self, client_id: u32) {
         while let Some(d) = self.rx.recv().await {
-            let start = Instant::now();
             if let Err(e) =
                 self.client
                     .try_publish(d.topic(), QoS::AtLeastOnce, false, d.serialized())
             {
-                error!("{client_id}: {e}");
+                error!("{client_id}: {e}; topic={}", d.topic());
             }
-            debug!(
-                "client_id: {client_id}; timespent: {}",
-                start.elapsed().as_secs_f64()
-            );
         }
     }
 }
