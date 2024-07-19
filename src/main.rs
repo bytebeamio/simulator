4b4a045d9e859988f51717a3a18e5e2ae6e0ffe7
Devdutt Shenoi <devdutt@bytebeam.io>
2024-07-19 15:05:49 +0530

    track delayed data generation

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,24 @@
-use std::{
-    collections::HashMap,
-    env::{current_dir, var},
-    fs::File,
-    io::BufReader,
-    pin::Pin,
-    sync::Arc,
-    thread,
-    time::Duration,
-};
+use std::{collections::HashMap, env::var, fs::File, io::BufReader, sync::Arc, thread};
 
-use chrono::TimeDelta;
-use log::{debug, error, info, warn};
-use rand::{rngs::StdRng, SeedableRng};
+use log::{error, info};
 use rumqttc::{AsyncClient, Event, Incoming, MqttOptions};
 use serde::Deserialize;
+use simulator::single_device;
 use tokio::{
     runtime::Builder,
-    select, spawn,
-    sync::mpsc::{channel, Receiver, Sender},
+    sync::mpsc::{channel, Receiver},
     task::JoinSet,
-    time::{interval, sleep, sleep_until, Instant, Sleep},
 };
 use tracing_subscriber::EnvFilter;
 
 mod data;
 mod mqtt;
 mod serializer;
+mod simulator;
 
 use data::{
-    ActionResult, Can, Data, DeviceShadow, Historical, Imu, Payload, PayloadArray, RideDetail,
-    RideStatistics, RideSummary, Stop, Type, VehicleLocation, VehicleState, VicRequest,
+    ActionResult, Can, Data, Historical, Imu, PayloadArray, RideDetail, RideStatistics,
+    RideSummary, Stop, VehicleLocation, VehicleState, VicRequest,
 };
 use mqtt::{push_mqtt_metrics, Mqtt};
 use serializer::{push_serializer_metrics, Serializer};
@@ -130,6 +119,11 @@ fn main() {
                     "/tenants/{}/devices/1/events/simulator_mqtt_metrics/jsonarray",
                     mqtt_config.project_id
                 );
+                tasks.spawn(push_mqtt_metrics(topic, client.clone()));
+                let topic = format!(
+                    "/tenants/{}/devices/1/events/simulator_data_metrics/jsonarray",
+                    mqtt_config.project_id
+                );
                 tasks.spawn(push_mqtt_metrics(topic, client));
 
                 while let Some(Err(e)) = tasks.join_next().await {
@@ -216,257 +210,4 @@ async fn start_mqtt_connection(
 
     let mut serializer = Serializer { rx, client };
     tasks.spawn(async move { serializer.start(client_id).await });
-}
-
-async fn batch_data(
-    mut rx: Receiver<Payload>,
-    tx: Sender<PayloadArray>,
-    topic: String,
-    max_buf_size: usize,
-    timeout: Duration,
-    compression: bool,
-) {
-    let mut data_array = PayloadArray {
-        topic: topic.to_owned(),
-        points: vec![],
-        compression,
-    };
-    let mut end: Pin<Box<Sleep>> = Box::pin(sleep(Duration::from_secs(u64::MAX)));
-    let mut push = None;
-    loop {
-        select! {
-            Some(payload) = rx.recv() => {
-                if data_array.points.is_empty() {
-                    push = Some( Box::pin(sleep(timeout)))
-                }
-                data_array.points.push(payload);
-                if data_array.points.len() < max_buf_size {
-                    continue
-                }
-            }
-            _ = &mut push.as_mut().unwrap_or(&mut end) => {
-                let wait = push.take().unwrap();
-                let elapsed = wait.deadline().elapsed();
-                if elapsed > Duration::from_millis(500) {
-                    warn!("Waited {}s longer than expected to batch for {topic}", elapsed.as_secs_f32());
-                }
-            }
-        }
-
-        if let Err(e) = tx.try_send(data_array.take()) {
-            error!("{e}; topic={topic}");
-        }
-        data_array.points.clear();
-    }
-}
-
-async fn push_data(
-    batch_tx: Sender<PayloadArray>,
-    project_id: String,
-    client_id: u32,
-    stream: &str,
-    max_buf_size: usize,
-    timeout: Duration,
-    compression: bool,
-    data: Arc<Historical>,
-) {
-    let mut last_time = None;
-    let mut sequence = 0;
-    let mut total_time = 0.0;
-
-    let mut data_path = current_dir().unwrap();
-    data_path.push(format!("data/{stream}"));
-
-    let mut rng = StdRng::from_entropy();
-    let mut iter = data.get_random(stream, &mut rng).iter();
-    let mut start = Instant::now();
-
-    let (tx, rx) = channel(1);
-    let mut topic = format!("/tenants/{project_id}/devices/{client_id}/events/{stream}/jsonarray");
-    if compression {
-        topic.push_str("/lz4")
-    }
-    spawn(batch_data(
-        rx,
-        batch_tx,
-        topic,
-        max_buf_size,
-        timeout,
-        compression,
-    ));
-
-    loop {
-        let rec: &Box<dyn Type> = match iter.next() {
-            Some(r) => r,
-            _ => {
-                iter = data.get_random(stream, &mut rng).iter();
-                continue;
-            }
-        };
-        if let Some(start) = last_time {
-            let diff: TimeDelta = rec.timestamp() - start;
-            let duration = diff.abs().to_std().unwrap();
-            let deadline = Instant::now() + duration;
-            sleep_until(deadline).await;
-
-            let elapsed = deadline.elapsed();
-            if elapsed > Duration::from_millis(500) {
-                warn!(
-                    "Waited longer than expected to generate {stream} for {client_id}by {}s",
-                    elapsed.as_secs_f32()
-                );
-            }
-        }
-        last_time = Some(rec.timestamp());
-
-        sequence %= u32::MAX;
-        sequence += 1;
-        if let Err(e) = tx.try_send(rec.payload(sequence)) {
-            error!("{e}")
-        }
-
-        total_time += start.elapsed().as_secs_f64();
-        debug!(
-            "client_id: {client_id}; Messages: {sequence}; Avg time: {}",
-            total_time / sequence as f64
-        );
-        start = Instant::now();
-    }
-}
-
-async fn single_device(
-    client_id: u32,
-    config: Arc<Config>,
-    tx: Sender<PayloadArray>,
-    data: Arc<Historical>,
-) {
-    info!("Simulating device {client_id}");
-
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "C2C_CAN",
-        100,
-        Duration::from_secs(60),
-        true,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "imu_sensor",
-        100,
-        Duration::from_secs(60),
-        true,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "action_result",
-        1,
-        Duration::from_secs(1),
-        false,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "ride_detail",
-        1,
-        Duration::from_secs(1),
-        false,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "ride_summary",
-        1,
-        Duration::from_secs(1),
-        false,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "ride_statistics",
-        1,
-        Duration::from_secs(1),
-        false,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "stop",
-        10,
-        Duration::from_secs(10),
-        false,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "vehicle_location",
-        10,
-        Duration::from_secs(10),
-        false,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "vehicle_state",
-        1,
-        Duration::from_secs(1),
-        false,
-        data.clone(),
-    ));
-    spawn(push_data(
-        tx.clone(),
-        config.project_id.clone(),
-        client_id,
-        "vic_request",
-        1,
-        Duration::from_secs(1),
-        false,
-        data.clone(),
-    ));
-
-    let mut sequence = 0;
-    let mut interval = interval(Duration::from_secs(10));
-
-    loop {
-        let start = Instant::now();
-        interval.tick().await;
-        let elapsed = start.elapsed();
-        if elapsed > Duration::from_millis(1500) {
-            warn!(
-                "Waited longer than expected to generate device shadow for {client_id} by {}s",
-                elapsed.as_secs_f32()
-            );
-        }
-        let data_array = PayloadArray {
-            topic: format!(
-                "/tenants/{}/devices/{client_id}/events/device_shadow/jsonarray",
-                config.project_id
-            ),
-            points: vec![DeviceShadow::default().payload(sequence)],
-            compression: false,
-        };
-        sequence += 1;
-        if let Err(e) = tx.try_send(data_array) {
-            error!("{client_id}/device_shadow: {e}");
-        }
-    }
 }
---
+++ src/simulator.rs
@@ -0,0 +1,316 @@
+use std::{
+    env::current_dir,
+    pin::Pin,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
+
+use chrono::{TimeDelta, Utc};
+use log::{debug, error, info, warn};
+use rand::{rngs::StdRng, SeedableRng};
+use rumqttc::v5::{mqttbytes::QoS, AsyncClient};
+use serde_json::json;
+use tokio::{
+    select, spawn,
+    sync::mpsc::{channel, Receiver, Sender},
+    time::{interval, sleep, sleep_until, Instant, Sleep},
+};
+
+use crate::{data::Data, Config};
+
+use super::data::{DeviceShadow, Historical, Payload, PayloadArray, Type};
+
+static mut DELAYED_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+async fn batch_data(
+    mut rx: Receiver<Payload>,
+    tx: Sender<PayloadArray>,
+    topic: String,
+    max_buf_size: usize,
+    timeout: Duration,
+    compression: bool,
+) {
+    let mut data_array = PayloadArray {
+        topic: topic.to_owned(),
+        points: vec![],
+        compression,
+    };
+    let mut end: Pin<Box<Sleep>> = Box::pin(sleep(Duration::from_secs(u64::MAX)));
+    let mut push = None;
+    loop {
+        select! {
+            Some(payload) = rx.recv() => {
+                if data_array.points.is_empty() {
+                    push = Some( Box::pin(sleep(timeout)))
+                }
+                data_array.points.push(payload);
+                if data_array.points.len() < max_buf_size {
+                    continue
+                }
+            }
+            _ = &mut push.as_mut().unwrap_or(&mut end) => {
+                let wait = push.take().unwrap();
+                let elapsed = wait.deadline().elapsed();
+                if elapsed > Duration::from_millis(500) {
+                    warn!("Waited {}s longer than expected to batch for {topic}", elapsed.as_secs_f32());
+                    unsafe {
+                        DELAYED_COUNT.fetch_add(1, Ordering::SeqCst);
+                    }
+                }
+            }
+        }
+
+        if let Err(e) = tx.try_send(data_array.take()) {
+            error!("{e}; topic={topic}");
+        }
+        data_array.points.clear();
+    }
+}
+
+async fn push_data(
+    batch_tx: Sender<PayloadArray>,
+    project_id: String,
+    client_id: u32,
+    stream: &str,
+    max_buf_size: usize,
+    timeout: Duration,
+    compression: bool,
+    data: Arc<Historical>,
+) {
+    let mut last_time = None;
+    let mut sequence = 0;
+    let mut total_time = 0.0;
+
+    let mut data_path = current_dir().unwrap();
+    data_path.push(format!("data/{stream}"));
+
+    let mut rng = StdRng::from_entropy();
+    let mut iter = data.get_random(stream, &mut rng).iter();
+    let mut start = Instant::now();
+
+    let (tx, rx) = channel(1);
+    let mut topic = format!("/tenants/{project_id}/devices/{client_id}/events/{stream}/jsonarray");
+    if compression {
+        topic.push_str("/lz4")
+    }
+    spawn(batch_data(
+        rx,
+        batch_tx,
+        topic,
+        max_buf_size,
+        timeout,
+        compression,
+    ));
+
+    loop {
+        let rec: &Box<dyn Type> = match iter.next() {
+            Some(r) => r,
+            _ => {
+                iter = data.get_random(stream, &mut rng).iter();
+                continue;
+            }
+        };
+        if let Some(start) = last_time {
+            let diff: TimeDelta = rec.timestamp() - start;
+            let duration = diff.abs().to_std().unwrap();
+            let deadline = Instant::now() + duration;
+            sleep_until(deadline).await;
+
+            let elapsed = deadline.elapsed();
+            if elapsed > Duration::from_millis(500) {
+                warn!(
+                    "Waited longer than expected to generate {stream} for {client_id}by {}s",
+                    elapsed.as_secs_f32()
+                );
+                unsafe {
+                    DELAYED_COUNT.fetch_add(1, Ordering::SeqCst);
+                }
+            }
+        }
+        last_time = Some(rec.timestamp());
+
+        sequence %= u32::MAX;
+        sequence += 1;
+        if let Err(e) = tx.try_send(rec.payload(sequence)) {
+            error!("{e}")
+        }
+
+        total_time += start.elapsed().as_secs_f64();
+        debug!(
+            "client_id: {client_id}; Messages: {sequence}; Avg time: {}",
+            total_time / sequence as f64
+        );
+        start = Instant::now();
+    }
+}
+
+pub async fn single_device(
+    client_id: u32,
+    config: Arc<Config>,
+    tx: Sender<PayloadArray>,
+    data: Arc<Historical>,
+) {
+    info!("Simulating device {client_id}");
+
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "C2C_CAN",
+        100,
+        Duration::from_secs(60),
+        true,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "imu_sensor",
+        100,
+        Duration::from_secs(60),
+        true,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "action_result",
+        1,
+        Duration::from_secs(1),
+        false,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "ride_detail",
+        1,
+        Duration::from_secs(1),
+        false,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "ride_summary",
+        1,
+        Duration::from_secs(1),
+        false,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "ride_statistics",
+        1,
+        Duration::from_secs(1),
+        false,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "stop",
+        10,
+        Duration::from_secs(10),
+        false,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "vehicle_location",
+        10,
+        Duration::from_secs(10),
+        false,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "vehicle_state",
+        1,
+        Duration::from_secs(1),
+        false,
+        data.clone(),
+    ));
+    spawn(push_data(
+        tx.clone(),
+        config.project_id.clone(),
+        client_id,
+        "vic_request",
+        1,
+        Duration::from_secs(1),
+        false,
+        data.clone(),
+    ));
+
+    let mut sequence = 0;
+    let mut interval = interval(Duration::from_secs(10));
+
+    loop {
+        let start = Instant::now();
+        interval.tick().await;
+        let elapsed = start.elapsed();
+        if elapsed > Duration::from_millis(1500) {
+            warn!(
+                "Waited longer than expected to generate device shadow for {client_id} by {}s",
+                elapsed.as_secs_f32()
+            );
+            unsafe {
+                DELAYED_COUNT.fetch_add(1, Ordering::SeqCst);
+            }
+        }
+        let data_array = PayloadArray {
+            topic: format!(
+                "/tenants/{}/devices/{client_id}/events/device_shadow/jsonarray",
+                config.project_id
+            ),
+            points: vec![DeviceShadow::default().payload(sequence)],
+            compression: false,
+        };
+        sequence += 1;
+        if let Err(e) = tx.try_send(data_array) {
+            error!("{client_id}/device_shadow: {e}");
+        }
+    }
+}
+
+pub async fn push_simulator_metrics(topic: String, client: AsyncClient) {
+    let mut interval = interval(Duration::from_secs(10));
+    let mut sequence = 0;
+    loop {
+        interval.tick().await;
+        let delayed = unsafe { DELAYED_COUNT.swap(0, Ordering::Acquire) };
+        debug!("delayed: {delayed}");
+        sequence += 1;
+        let payload = PayloadArray {
+            topic: topic.clone(),
+            points: vec![Payload {
+                sequence,
+                timestamp: Utc::now(),
+                payload: json!({
+                    "delayed": delayed
+                }),
+            }],
+            compression: false,
+        };
+        if let Err(e) = client
+            .publish(&topic, QoS::AtLeastOnce, false, payload.serialized())
+            .await
+        {
+            error!("{e}; topic={topic}")
+        };
+    }
+}
